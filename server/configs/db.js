8c80275f78e4fddc3f63649605cf6a6c7dd0b97d
Israk Reza <israk427@gmail.com>
2026-07-12 05:07:39 +0600

    added ingest

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,1 +1,12 @@
 import mongoose from "mongoose";
+
+const connectDb = async ()=>{
+    try {
+        mongoose.connection.on('connected', ()=> console.log('Database Connected'));
+        await mongoose.connect(`${process.env.MONGODB_URI}/quickshow`)
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export default connectDb;
