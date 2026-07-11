d4d3472889ae00a9a5e100e48cf02816f9052065
Israk Reza <israk427@gmail.com>
2026-07-12 04:20:46 +0600

    backend files

--- client/src/pages/admin/Dashboard.jsx
+++ client/src/pages/admin/Dashboard.jsx
@@ -1,9 +1,10 @@
-import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, UsersIcon } from 'lucide-react';
+import { ChartLineIcon, CircleDollarSignIcon, PlayCircleIcon, StarIcon, UsersIcon } from 'lucide-react';
 import React, { useEffect, useState } from 'react'
 import { dummyDashboardData } from '../../assets/assets';
 import Loading from '../../components/Loading';
 import Title from '../../components/admin/Title';
 import BlurCircle from '../../components/BlurCircle';
+import { dateFormat } from '../../lib/dateFormat';
 
 const Dashboard = () => {
 
@@ -19,7 +20,7 @@ const Dashboard = () => {
 
     const dashboardCards = [
         {title: "Total Bookings", value: dashboardData.totalBookings || "0", icon: ChartLineIcon},
-        {title: "Total Revenue", value: dashboardData.totalRevenue || "0", icon: CircleDollarSignIcon},
+        {title: "Total Revenue", value: currency + dashboardData.totalRevenue || "0", icon: CircleDollarSignIcon},
         { title: "Active Shows", value: Array.isArray(dashboardData.activeShows) ? dashboardData.activeShows.length : (dashboardData.activeShows || 0), icon: PlayCircleIcon },
         {title: "Total Users", value: dashboardData.totalUser || "0", icon: UsersIcon}
     ]
@@ -57,6 +58,26 @@ const Dashboard = () => {
                     );
                 })}
         </div>
+      </div>
+
+      <p className='mt-10 text-lg font-medium'>Active Shows</p>
+      <div className='relative flex flex-wrap gap-6 mt-4 max-w-5xl'>
+        <BlurCircle top='100px' left='-10%'/>
+        {dashboardData.activeShows.map((show) => (
+            <div key={show._id} className='w-55 rounded-lg overflow-hidden h-full pb-3 bg-primary/10 border border-primary/20
+            hover:-translate-y-1 transition duration-300'>
+                <img src={show.movie.poster_path} alt="" className='h-60 w-full object-cover'/>
+                <p className='font-medium p-2 truncate'>{show.movie.title}</p>
+                <div className='flex items-center justify-between px-2'>
+                    <p className='text-lg font-medium'>{currency} {show.showPrice}</p>
+                    <p className='flex items-center gap-1 text-sm text-gray-400 mt-1 pr-1'>
+                        <StarIcon className='w-4 h-4 text-primary fill-primary'/>
+                        {show.movie.vote_average.toFixed(1)}
+                    </p>
+                </div>
+                <p className='px-2 pt-2 text-sm text-gray-500'>{dateFormat(show.showDateTime)}</p>
+            </div>
+        ))}
       </div>
     </>
   ) : <Loading />
